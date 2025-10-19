2307b120c8d75a581af8f8b1d6501f8ae6e62861
miqueiassantoss <email>
2025-10-19 17:52:25 -0300

    tip-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Header } from "./components/Header"
 import styles from "./App.module.css"
 
+import { Header } from "./components/Header"
+import { Tip } from "./components/Tip"
+
+
 export default function App() {
+
+  function handleRestarGame(){
+    confirm("Reiniciar o jogo?")
+  }
+
   return (
     <div className={styles.container}>
       <main>
-        <Header />
+        <Header current={5} max={10} onRestart={handleRestarGame}/>
+
+        <Tip tip="Linguagem de programação dinâmica"/>
       </main>
     </div>
   )
