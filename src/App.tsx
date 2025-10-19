ff499423704a5e1e8d5f3e4a1c1e6be0d5bd1718
miqueiassantoss <email>
2025-10-19 18:12:29 -0300

    creating-main

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./App.module.css"
 
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 
 export default function App() {
@@ -16,6 +17,14 @@ export default function App() {
         <Header current={5} max={10} onRestart={handleRestarGame}/>
 
         <Tip tip="Linguagem de programação dinâmica"/>
+
+        <div className={styles.word}>
+          <Letter value ="R"/>
+          <Letter value ="e"/>
+          <Letter value ="a"/>
+          <Letter value ="c"/>
+          <Letter value ="t"/>
+        </div>
       </main>
     </div>
   )
